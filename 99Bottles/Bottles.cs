8f06c52903c16cf7ac60a474a2d5c8531d1be84b
Vicenç García Altés <vincentredrum@gmail.com>
2016-08-07 22:05:02 +0100

    Initial commit

---
+++ 99Bottles/Bottles.cs
@@ -0,0 +1,20 @@
+﻿namespace _99Bottles
+{
+    public class Bottles
+    {
+        public string Verse(int verseNumber)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Verses(int starting, int ending)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Song()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
