27f8a3b05fc3a068539602074a45d6345386f2ea
Vicenç García Altés <vincentredrum@gmail.com>
2016-08-08 16:58:40 +0100

    Refactor to classes.

--- 99Bottles/Bottles.cs
+++ 99Bottles/Bottles.cs
@@ -1,28 +1,72 @@
 ﻿namespace _99Bottles
 {
-    using System.Runtime.InteropServices.WindowsRuntime;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+
+    public interface IVerse
+    {
+        string Text { get; }
+    }
+
+    public class CommonVerse : IVerse
+    {
+        private readonly int _numVerse;
+
+        public CommonVerse(int numVerse)
+        {
+            _numVerse = numVerse;
+        }
+
+        public string Text => $"{_numVerse} bottles of beer on the wall, " +
+                              $"{_numVerse} bottles of beer.\n" +
+                              "Take one down and pass it around, " +
+                              $"{_numVerse - 1} bottles of beer on the wall.\n";
+    }
+
+    public class VerseTwo : IVerse
+    {
+        public string Text => $"2 bottles of beer on the wall, " +
+                              $"2 bottles of beer.\n" +
+                              "Take one down and pass it around, " +
+                              $"1 bottle of beer on the wall.\n";
+    }
+
+    public class VerseOne : IVerse
+    {
+        public string Text => "1 bottle of beer on the wall, " +
+                              "1 bottle of beer.\n" +
+                              "Take it down and pass it around, " +
+                              $"no more bottles of beer on the wall.\n";
+    }
+
+    public class VerseZero : IVerse
+    {
+        public string Text => "No more bottles of beer on the wall, " +
+                              "no more bottles of beer.\n" +
+                              "Go to the store and buy some more, " +
+                              "99 bottles of beer on the wall.\n";
+    }
+
+
 
     public class Bottles
     {
+        private List<IVerse> verses;
+         
+        public Bottles()
+        {
+            verses = new List<IVerse> {new VerseZero(), new VerseOne(), new VerseTwo()};
+
+            for (var i = 3; i <= 99; i++)
+            {
+                verses.Add(new CommonVerse(i));
+            }
+        }
+
         public string Verse(int verseNumber)
         {
-            if (verseNumber == 0)
-            {
-                return "No more bottles of beer on the wall, " +
-                           "no more bottles of beer.\n" +
-                           "Go to the store and buy some more, " +
-                           "99 bottles of beer on the wall.\n";
-            }
-
-            var plural = verseNumber > 1 ? "s" : "";
-            var nextPlural = verseNumber - 1 != 1 ? "s" : "";
-            var nextVerseNumber = verseNumber == 1 ? "no more" : $"{verseNumber - 1}";
-            var oneOrIt = verseNumber == 1 ? "it" : "one";
-
-            return $"{verseNumber} bottle{plural} of beer on the wall, " +
-                   $"{verseNumber} bottle{plural} of beer.\n" +
-                   $"Take {oneOrIt} down and pass it around, " +
-                   $"{nextVerseNumber} bottle{nextPlural} of beer on the wall.\n";
+            return verses.ElementAt(verseNumber).Text;
         }
 
         public string Verses(int starting, int ending)
