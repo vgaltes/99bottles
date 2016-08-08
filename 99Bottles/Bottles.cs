6c93a16fed2387fe19ed063298ff98c2900e1b74
Vicenç García Altés <vincentredrum@gmail.com>
2016-08-08 16:27:55 +0100

    Single verses ok

--- 99Bottles/Bottles.cs
+++ 99Bottles/Bottles.cs
@@ -4,12 +4,23 @@
     {
         public string Verse(int verseNumber)
         {
+            if (verseNumber == 0)
+            {
+                return "No more bottles of beer on the wall, " +
+                           "no more bottles of beer.\n" +
+                           "Go to the store and buy some more, " +
+                           "99 bottles of beer on the wall.\n";
+            }
+
             var plural = verseNumber > 1 ? "s" : "";
-            var nextPlural = verseNumber - 1 > 1 ? "s" : "";
+            var nextPlural = verseNumber - 1 != 1 ? "s" : "";
+            var nextVerseNumber = verseNumber == 1 ? "no more" : $"{verseNumber - 1}";
+            var oneOrIt = verseNumber == 1 ? "it" : "one";
+
             return $"{verseNumber} bottle{plural} of beer on the wall, " +
                    $"{verseNumber} bottle{plural} of beer.\n" +
-                   "Take one down and pass it around, " +
-                   $"{verseNumber - 1} bottle{nextPlural} of beer on the wall.\n";
+                   $"Take {oneOrIt} down and pass it around, " +
+                   $"{nextVerseNumber} bottle{nextPlural} of beer on the wall.\n";
         }
 
         public string Verses(int starting, int ending)
